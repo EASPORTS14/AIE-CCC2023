7da0243dabcfce3fd5fed82b3f07097967e45ba4
Lizongze <lzz19260817@outlook.com>
2023-07-06 22:04:37 +0800

    for test

--- myFilter2D/aie/src/graph.cpp
+++ myFilter2D/aie/src/graph.cpp
@@ -7,9 +7,6 @@ using namespace adf;
 
 // Graph object
 Filter2DGraph filter_graph;
-simulation::platform<1,1> platform("data/input.txt", "data/output_vectorized.txt");
-connect<> net0(platform.src[0], filter_graph.in);
-connect<> net1(filter_graph.out, platform.sink[0]);
 
 int main(void) {
   filter_graph.init();
