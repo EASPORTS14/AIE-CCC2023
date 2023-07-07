5fc9f05f9f4344d09b56e611e3b0e37e2584a9c2
Lizongze <lzz19260817@outlook.com>
2023-07-07 22:12:52 +0800

    for test

--- myFilter2D/aie/src/graph.h
+++ myFilter2D/aie/src/graph.h
@@ -22,14 +22,14 @@ class Filter2DGraph : public adf::graph {
             // create kernel
             f2d = kernel::create(filter2D);
 
-            in = input_plio::create("DataIn1", plio_128_bits, "data/input.txt");
-            out = output_plio::create("DataOut1", plio_128_bits, "data/output.txt");
+            // in = input_plio::create("DataIn1", plio_128_bits, "data/input.txt");
+            // out = output_plio::create("DataOut1", plio_128_bits, "data/output.txt");
 
             //Make AIE connections
             // adf::connect<window<TILE_WINDOW_SIZE> >(in.out[0], f2d.in[0]);
             // adf::connect<window<TILE_WINDOW_SIZE> >(f2d.out[0], out.in[0]);
-            connect< window<TILE_WINDOW_SIZE> > net0 (in.out[0], f2d.in[0]);
-            connect< window<TILE_WINDOW_SIZE> > net1 (f2d.out[0], out.in[0]);
+            connect< window<TILE_WINDOW_SIZE> > net0 (in., f2d.in[0]);
+            connect< window<TILE_WINDOW_SIZE> > net1 (f2d.out[0], out);
 
             source(f2d) = "/aie_kernels/aie_filter2D.cpp";
             runtime<ratio>(f2d) = 1;
