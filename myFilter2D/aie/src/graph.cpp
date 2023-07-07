9e155354c725df2129ef5503436532187a37ea5e
Lizongze <lzz19260817@outlook.com>
2023-07-07 22:01:24 +0800

    for test

--- myFilter2D/aie/src/graph.cpp
+++ myFilter2D/aie/src/graph.cpp
@@ -6,8 +6,8 @@ using namespace adf;
 // Graph object
 Filter2DGraph filter_graph;
 simulation::platform<1,1> platform("data/input.txt", "data/output.txt");
-connect<> net0(platform.src[0], mygraph.in);
-connect<> net1(mygraph.out, platform.sink[0]);
+connect<> net0(platform.src[0], filter_graph.in);
+connect<> net1(filter_graph.out, platform.sink[0]);
 
 int main(void) {
   filter_graph.init();
