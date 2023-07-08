ed25471ffc80c045927e1ea5ed4093cc223982ec
Lizongze <lzz19260817@outlook.com>
2023-07-08 23:09:30 +0800

    for test

--- myFilter2D/aie/src/graph.cpp
+++ myFilter2D/aie/src/graph.cpp
@@ -5,9 +5,9 @@ using namespace adf;
 
 // Graph object
 Filter2DGraph filter_graph;
-simulation::platform<1,1> platform("data/input.txt", "data/output.txt");
-connect<> net0(platform.src[0], filter_graph.in);
-connect<> net1(filter_graph.out, platform.sink[0]);
+// simulation::platform<1,1> platform("data/input.txt", "data/output.txt");
+// connect<> net0(platform.src[0], filter_graph.in);
+// connect<> net1(filter_graph.out, platform.sink[0]);
 
 int main(void) {
   filter_graph.init();
--- myFilter2D/aie/src/graph.h
+++ myFilter2D/aie/src/graph.h
@@ -12,17 +12,17 @@ class Filter2DGraph : public adf::graph {
     private:
         kernel f2d;
     public:
-        // input_plio in;
-        // output_plio out;
-        input_port in;
-        output_port out;
+        input_plio in;
+        output_plio out;
+        // input_port in;
+        // output_port out;
 
         Filter2DGraph() {
             // create kernel
             f2d = kernel::create(filter2D);
 
-            // in = input_plio::create("DataIn1", plio_128_bits, "data/input.txt");
-            // out = output_plio::create("DataOut1", plio_128_bits, "data/output.txt");
+            in = input_plio::create("DataIn1", plio_128_bits, "data/input.txt");
+            out = output_plio::create("DataOut1", plio_128_bits, "data/output.txt");
 
             //Make AIE connections
             // adf::connect<window<TILE_WINDOW_SIZE> >(in.out[0], f2d.in[0]);
